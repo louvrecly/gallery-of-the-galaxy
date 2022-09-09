8539597f50ba578bf6497bedcbf39a8d4467d48b
Louvre Chow <louvrecly@gmail.com>
2022-09-09 18:21:30 -0400

    enh/performance: Remove comments in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,6 @@ export default {
     base: '/gallery-of-the-galaxy'
   },
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: '🪐 gallery-of-the-galaxy',
     meta: [
@@ -16,47 +15,35 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@fortawesome/fontawesome-svg-core/styles.css',
     '~/assets/css/main'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/fontawesome.js'
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
     { path: '~/components', extensions: ['vue'], pathPrefix: false }
   ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module'
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     'cookie-universal-nuxt',
     'nuxt-client-init-module'
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: process.env.NASA_API_URL
   },
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       lang: 'en'
@@ -67,7 +54,6 @@ export default {
     '/spacestagram-vue': '~/serverMiddleware/redirectRoute.js'
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   publicRuntimeConfig: {
     nasaApiKey: process.env.NASA_API_KEY || 'DEMO_KEY'
