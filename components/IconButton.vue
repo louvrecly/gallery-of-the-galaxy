b69432db00e60489c116ac493f9c1f552734a690
Louvre Chow <louvrecly@gmail.com>
2022-09-09 15:02:53 -0400

    enh/component: Create IconButton

---
+++ components/IconButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="icon-button icon-button" @click="onClick">
+    <FontAwesomeIcon :icon="[iconType, iconName]" class="icon" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      required: true
+    },
+    iconType: {
+      type: String,
+      default: 'far'
+    }
+  },
+  methods: {
+    onClick (e) {
+      this.$emit('click', e)
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.icon-button
+  .icon
+    height: 20px
+</style>
