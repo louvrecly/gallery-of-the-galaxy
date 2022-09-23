82fab94a39dfaf1b6b540f7d0e9b54603da7950b
Louvre Chow <louvrecly@gmail.com>
2022-09-22 20:10:31 -0400

    enh/store-refactor: Replace posts and startDate states with postsMap and dates, refactor getPosts and getStartDate getters, replace SET_POSTS and SET_START_DATE mutations with ADD_TO_POSTS_MAP and UPDATE_DATES and refactor loadPosts and loadPost actions

--- store/index.js
+++ store/index.js
@@ -1,14 +1,14 @@
 export const state = () => ({
   busy: false,
-  posts: [],
-  startDate: null,
+  postsMap: {},
+  dates: [],
   likedPostDates: []
 })
 
 export const getters = {
   getBusyState: state => state.busy,
-  getPosts: state => state.posts,
-  getStartDate: state => state.startDate,
+  getPosts: state => state.dates.map(date => state.postsMap[date]),
+  getStartDate: state => state.dates[state.dates.length - 1],
   getLikePostDates: state => state.likedPostDates
 }
 
@@ -16,11 +16,14 @@ export const mutations = {
   SET_BUSY_STATE (state, busy) {
     state.busy = busy
   },
-  SET_POSTS (state, posts) {
-    state.posts = posts
+  ADD_TO_POSTS_MAP (state, postsMap) {
+    state.postsMap = { ...state.postsMap, ...postsMap }
   },
-  SET_START_DATE (state, startDate) {
-    state.startDate = startDate
+  UPDATE_DATES (state) {
+    const dates = Object.keys(state.postsMap)
+    if (state.dates.length === dates.length) return
+    dates.sort((dateA, dateB) => dateA < dateB ? 1 : -1)
+    state.dates = dates
   },
   SET_LIKED_POST_DATES (state, likedPostDates) {
     state.likedPostDates = likedPostDates
@@ -38,31 +41,34 @@ export const actions = {
     const likedPostDates = app.$cookies.get('liked-post-dates')
     if (likedPostDates) commit('SET_LIKED_POST_DATES', likedPostDates)
   },
-  async loadPosts ({ commit, state }, { startDate, endDate, nasaApiKey }) {
-    if (state.startDate === startDate && state.posts.length) return state.posts
+  async loadPosts ({ commit }, { startDate, endDate, nasaApiKey }) {
+    if (startDate === endDate) return
 
     commit('SET_BUSY_STATE', true)
 
-    const posts = await this.$axios.$get('/planetary/apod', {
+    const newPosts = await this.$axios.$get('/planetary/apod', {
       params: {
         api_key: nasaApiKey,
         start_date: startDate,
-        end_date: endDate
+        end_date: endDate,
+        thumbs: true
       }
     })
 
-    const sortedNewPosts = posts.sort((postA, postB) => postA.date < postB.date ? 1 : -1)
-    const sortedPosts = [...state.posts, ...sortedNewPosts]
+    const newPostsMap = newPosts.reduce(
+      (subMap, post) => ({ ...subMap, [post.date]: post }),
+      {}
+    )
 
-    commit('SET_START_DATE', startDate)
-    commit('SET_POSTS', sortedPosts)
+    commit('ADD_TO_POSTS_MAP', newPostsMap)
+    commit('UPDATE_DATES')
+
     commit('SET_BUSY_STATE', false)
-    return sortedPosts
   },
   async loadPost ({ commit, state }, { date, nasaApiKey }) {
     if (!date) return
 
-    let post = state.posts.find(post => post.date === date)
+    let post = state.postsMap[date]
     if (post) return post
 
     commit('SET_BUSY_STATE', true)
@@ -73,6 +79,9 @@ export const actions = {
         date
       }
     })
+
+    commit('ADD_TO_POSTS_MAP', { [post.date]: post })
+    commit('UPDATE_DATES')
 
     commit('SET_BUSY_STATE', false)
 
