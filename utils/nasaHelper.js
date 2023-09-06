c67bcb1fe85ea596334d0fb6b2d4569ba9482848
Louvre Chow <louvrecly@gmail.com>
2023-09-06 18:50:09 -0400

    enh/store-refactor: rename constants and variables

--- utils/nasaHelper.js
+++ utils/nasaHelper.js
@@ -1,18 +1,18 @@
 import moment from 'moment'
 
-const oldestStartDate = '2021-01-01' // oldest available start date: '1995-06-16'
-const defaultNumOfPosts = 14
-const oldestStartDateMoment = moment(oldestStartDate)
+const EARLIEST_START_DATE = '2021-01-01' // earliest available start date: '1995-06-16'
+const DEFAULT_DAYS_COUNT = 14
+const earliestStartDateMoment = moment(EARLIEST_START_DATE)
 
 export function validateDateString(dateString = '') {
   const date = moment(dateString)
-  return date.isValid() && date.isBetween(oldestStartDate, moment(), 'day', '[]')
+  return date.isValid() && date.isBetween(EARLIEST_START_DATE, moment(), 'day', '[]')
 }
 
-export function getNextDateRange (oldStartDate) {
+export function getNextDateRange(oldStartDate) {
   const endDateMoment = oldStartDate ? moment(oldStartDate).subtract(1, 'days') : moment()
-  const numOfDays = Math.min(endDateMoment.diff(oldestStartDateMoment, 'days'), defaultNumOfPosts)
+  const daysCount = Math.min(endDateMoment.diff(earliestStartDateMoment, 'days'), DEFAULT_DAYS_COUNT)
   const endDate = endDateMoment.format('YYYY-MM-DD')
-  const startDate = moment(endDate).subtract(numOfDays, 'days').format('YYYY-MM-DD')
+  const startDate = moment(endDate).subtract(daysCount, 'days').format('YYYY-MM-DD')
   return { startDate, endDate }
 }
